8ddfb4d5953c34af80478dd6a6c89921fd018c81
pho-veteran <thanhvinhlqt2016@gmail.com>
2023-12-26 12:16:00 +0700

    Add userDashboard Components

--- src/GUI/Dashboard/User/userDashboard.java
+++ src/GUI/Dashboard/User/userDashboard.java
@@ -1,7 +1,10 @@
 package GUI.Dashboard.User;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class userDashboard extends JFrame {
     private JPanel mainPanel;
@@ -12,9 +15,41 @@ public class userDashboard extends JFrame {
     private JPanel profileInfo;
     private JSeparator separator1;
     private JPanel leftPanel;
-
+    private JLabel logoLabel;
+    private JLabel usernameLabel;
+    private JLabel balanceLabel;
+    private JLabel primeStatus;
+    private JPanel inlineInfo;
+    private JPanel contentNaviPane;
+    private ImageIcon imageIcon = new ImageIcon(new ImageIcon("src/icon/logo.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
     public userDashboard() {
         this.initFrame();
+        storeNavigator.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                storeNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#0EB194")));
+                libraryNavigator.setBorder(null);
+                walletNavigator.setBorder(null);
+            }
+        });
+
+        libraryNavigator.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                storeNavigator.setBorder(null);
+                walletNavigator.setBorder(null);
+                libraryNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#0EB194")));
+            }
+        });
+
+        walletNavigator.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                storeNavigator.setBorder(null);
+                libraryNavigator.setBorder(null);
+                walletNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#0EB194")));
+            }
+        });
     }
     public void initFrame() {
         this.setTitle("User Dashboard");
@@ -22,11 +57,10 @@ public class userDashboard extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
-        storeNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#546de5")));
+        logoLabel.setIcon(imageIcon);
+//        storeNavigator.setBorder(BorderFactory.createLineBorder(Color.decode("#0EB194"), 3, true));
+        storeNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#0EB194")));
         storeNavigator.setBorderPainted(true);
-        libraryNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#546de5")));
-        libraryNavigator.setBorderPainted(true);
-        walletNavigator.setBorder(BorderFactory.createMatteBorder(0, 5, 0, 0, Color.decode("#546de5")));
     }
     public static void main(String[] args) {
         new userDashboard().setVisible(true);
